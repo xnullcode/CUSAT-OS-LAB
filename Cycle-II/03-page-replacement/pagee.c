3ce93b6eb39935213205cb5ca8b14566c41af087
Adil Ali <92659803+xnullcode@users.noreply.github.com>
2026-05-03 21:24:52 +0530

    Refactor page replacement logic for clarity

--- Cycle-II/03-page-replacement/pagee.c
+++ Cycle-II/03-page-replacement/pagee.c
@@ -4,35 +4,35 @@
 	    int lastUsed[100];
 	    int faults = 0;
 	    for (int i = 0; i < capacity; i++) {
-		frames[i] = -1;
-		lastUsed[i] = -1;
+			frames[i] = -1;
+			lastUsed[i] = -1;
 	    }
 	    for (int i = 0; i < n; i++) {
-		int page = pages[i];
-		int found = 0;
-		for (int j = 0; j < capacity; j++) {
-		    if (frames[j] == page) {
-		        found = 1;
-		        lastUsed[j] = i;
-		        break;
-		    }
+			int page = pages[i];
+			int found = 0;
+			for (int j = 0; j < capacity; j++) {
+			    if (frames[j] == page) {
+			        found = 1;
+			        lastUsed[j] = i;
+			        break;
+			    }
+			}
+	   		if (!found) {
+		  		int lruIndex = 0;
+		 		for (int j = 0; j < capacity; j++) {
+					if (frames[j] == -1) {
+		  				lruIndex = j;
+						break;
+			        }
+			        if (lastUsed[j] < lastUsed[lruIndex]) {
+			            lruIndex = j;
+			        }
+			    }
+			    frames[lruIndex] = page;
+			    lastUsed[lruIndex] = i;
+			    faults++;
+			}
 		}
-	     if (!found) {
-		    int lruIndex = 0;
-		    for (int j = 0; j < capacity; j++) {
-		        if (frames[j] == -1) {
-		            lruIndex = j;
-		            break;
-		        }
-		        if (lastUsed[j] < lastUsed[lruIndex]) {
-		            lruIndex = j;
-		        }
-		    }
-		    frames[lruIndex] = page;
-		    lastUsed[lruIndex] = i;
-		    faults++;
-		}
-	    }
 	    return faults;
 	}
 	int main() {
@@ -44,11 +44,9 @@
 	    scanf("%d", &n);
 	    printf("Enter the %d page references:\n", n);
 	    for (int i = 0; i < n; i++) {
-		scanf("%d", &pages[i]);
+			scanf("%d", &pages[i]);
 	    }
-
 	    int result = pageFaults(pages, n, capacity);
 	    printf("Page faults: %d\n", result);
-
 	    return 0;
 	}
